e0e6653ed5975ff0e3fa26aae972773856176b8e
geslyn <61592906+geslyn@users.noreply.github.com>
2020-04-06 02:08:08 -0400

    Update excel_to_csv.py

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
 
-df = pd.read_excel ("‪C:\Users\Geslyn Andujar\Documents\COVID.xlsx", sheetname - "Sheet1")
+df = pd.read_excel ("‪C:\Users\Geslyn Andujar\Documents\COVID.xlsx, sheetname - "Sheet1")
 
 print(df)
